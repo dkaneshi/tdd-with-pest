84c2b73a1cebcb0bfbdb412539a00df2f3520887
dkaneshi <dkaneshi@gmail.com>
2025-06-19 21:15:45 -1000

    test: add separate test cases for retrieving book 1 and book 2 data from Books API

--- tests/Feature/BooksApiTest.php
+++ tests/Feature/BooksApiTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('retrieves the correct data from the books API', function () {
+it('retrieves the correct book 1 data from the books API', function () {
 
     // Arrange
 
@@ -17,6 +17,27 @@ it('retrieves the correct data from the books API', function () {
                 'id' => 1,
                 'name' => 'Robert C. Martin',
                 'bio' => 'This is an author'
+            ]
+        ]);
+});
+
+it('retrieves the correct book 2 data from the books API', function () {
+
+    // Arrange
+
+    // Act
+    $response = $this->json(method: 'GET', uri: '/books/2');
+
+    // Assert
+    expect($response->getStatusCode())->toBeInt()->toBe(200)
+        ->and($response->getBody())->toMatchJson([
+            'id' => 2,
+            'title' => 'Refadtoring: Improving the Design of Existing Code',
+            'year_published' => 1999,
+            'author' => [
+                'id' => 2,
+                'name' => 'Martin Fowler',
+                'bio' => 'Martin\'s bio'
             ]
         ]);
 });
