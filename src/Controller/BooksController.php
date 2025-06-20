3400fc588bff5e57a5b88e9ac1b55ecde09fc411
dkaneshi <dkaneshi@gmail.com>
2025-06-20 13:29:13 -1000

    feat: update `BooksController` to return dynamic book data based on ID
    - Add conditional logic to handle multiple book IDs in `show` method.
    - Return JSON-encoded response with appropriate HTTP status code.

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -9,9 +9,33 @@ class BooksController
 
     public function show(int $id): Response
     {
-        $body = '{"id":1,"title":"Clean Code: A Handbook of Agile Software Craftsmanship",
-            "year_published":2008,"author":{"id":1,"name":"Robert C. Martin","bio":"This is an author"}}';
+        $book = [];
 
-        return new Response(body: $body);
+        if ($id === 1) {
+            $book = [
+                'id' => 1,
+                'title' => 'Clean Code: A Handbook of Agile Software Craftsmanship',
+                'year_published' => 2008,
+                'author' => [
+                    'id' => 1,
+                    'name' => 'Robert C. Martin',
+                    'bio' => 'This is an author'
+                ]
+            ];
+        }
+
+        if ($id === 2) {
+            $book = [
+                'id' => 2,
+                'title' => 'Refadtoring: Improving the Design of Existing Code',
+                'year_published' => 1999,
+                'author' => [
+                    'id' => 2,
+                    'name' => 'Martin Fowler',
+                    'bio' => 'Martin\'s bio'
+                ]
+            ];
+        }
+        return new Response(json_encode($book), Response::HTTP_OK);
     }
 }
