5bf82230f6cb1b9e9b769522a4e4f9d02a174565
dkaneshi <dkaneshi@gmail.com>
2025-06-21 08:36:47 -1000

    feat: add dotenv integration and configure container services for environment variables
    - Integrate Symfony Dotenv component to load `.env` configuration.
    - Update service container to use `DSN` from environment variables.
    - Register `migrations_folder` as a new service in the container.

--- config/services.php
+++ config/services.php
@@ -4,24 +4,33 @@ use App\Database\Connection;
 use App\Http\Kernel;
 use App\Routing\RouteHandlerResolver;
 use App\Routing\Router;
+use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
 use League\Container\ReflectionContainer;
+use Symfony\Component\Dotenv\Dotenv;
 
 $container = new Container();
 
 $container->delegate(new ReflectionContainer(true));
+$dotenv = new Dotenv();
+$dotenv->load(dirname(__DIR__).'/.env');
 
 // parameters
-$dsn = 'sqlite:db/tdd-with-pest.sqlite';
-$container->add('dsn', new \League\Container\Argument\Literal\StringArgument($dsn));
+$dsn = $_ENV['DSN'];
+$container->add('dsn', new StringArgument($dsn));
 $routes = include __DIR__.'/routes.php';
+$migrationsFolder = dirname(__DIR__).'/migrations';
+$container->add(
+    'migrations_folder',
+    new StringArgument($migrationsFolder)
+);
 
 // services
 $container->add(RouteHandlerResolver::class)
     ->addArguments([$container]);
 
 $container->add(Router::class)
-    ->addArguments([\App\Routing\RouteHandlerResolver::class]);
+    ->addArguments([RouteHandlerResolver::class]);
 
 $container->extend(Router::class)
     ->addMethodCall('setRoutes', [$routes]);
